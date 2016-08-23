c18698ffe3ac1107bb8142af17c245e345fe75f6
Alex <darthsanches@gmail.com>
2016-08-23 21:02:07 +0300

    add server request

---
+++ app/src/main/java/com/darthsanches/rsswidget/FeedResponceTask.java
@@ -0,0 +1,55 @@
+package com.darthsanches.rsswidget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.darthsanches.rsswidget.reader.RssReader;
+import com.darthsanches.rsswidget.util.CommonStringsHelper;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alexandroid on 23.08.2016.
+ */
+public class FeedResponceTask extends AsyncTask<String, Integer, List<JSONObject>> {
+
+    private Context ctx;
+
+    public FeedResponceTask(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected void onPostExecute(List<JSONObject> jobs) {
+
+    }
+
+    @Override
+    protected List<JSONObject> doInBackground(String... arg0) {
+        List<JSONObject> jobs = new ArrayList<JSONObject>();
+
+        try {
+            CommonStringsHelper res = new CommonStringsHelper(ctx.getResources());
+            jobs = RssReader.getLatestRssFeed(res);
+        } catch (Exception e) {
+            Log.e("RSS ERROR", "Error loading RSS Feed Stream >> " + e.getMessage() + " //" + e.toString());
+        }
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(ctx);
+
+        MainWidget.updateWidget(ctx, appWidgetManager, jobs);
+
+        return jobs;
+    }
+}
+
--- app/src/main/java/com/darthsanches/rsswidget/MainWidget.java
+++ app/src/main/java/com/darthsanches/rsswidget/MainWidget.java
@@ -3,10 +3,23 @@ package com.darthsanches.rsswidget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
+
+import com.darthsanches.rsswidget.reader.RssReader;
+import com.darthsanches.rsswidget.util.CommonStringsHelper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by alexandroid on 22.08.2016.
@@ -16,40 +29,85 @@ public class MainWidget extends AppWidgetProvider {
     final static String ACTION_CHANGE = "ru.startandroid.develop.p1201clickwidget.change_count";
     final static String ACTION_OPEN_SETTINGS = "com.darthsanches.rsswidget.open_settings_action";
 
+    private List<JSONObject> jobs = new ArrayList<JSONObject>();
+
     boolean isSettings;
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-        int widgetID = appWidgetIds[0];
-        RemoteViews remoteViews;
-        if (isSettings) {
-            remoteViews = new RemoteViews(context.getPackageName(), R.layout.settings_screen);
-            Intent countIntent = new Intent(context, MainWidget.class);
-            countIntent.setAction(ACTION_CHANGE);
-            countIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
-            PendingIntent pIntent = PendingIntent.getBroadcast(context, widgetID, countIntent, 0);
-            remoteViews.setOnClickPendingIntent(R.id.button, pIntent);
-        } else {
-            remoteViews = new RemoteViews(context.getPackageName(), R.layout.main_widget_screen);
-            remoteViews.setTextViewText(R.id.title, "Bugaga!");
-            remoteViews.setTextViewText(R.id.text, "Bugagashenka!");
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgets) {
 
-/*            Intent countIntent = new Intent(context, MainWidget.class);
-            countIntent.setAction(ACTION_CHANGE);
-            countIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
-            PendingIntent pIntent = PendingIntent.getBroadcast(context, widgetID, countIntent, 0);
-            remoteViews.setOnClickPendingIntent(R.id.button_next, pIntent);*/
-
-            Intent settingsIntent = new Intent(context, MainWidget.class);
-            settingsIntent.setAction(ACTION_OPEN_SETTINGS);
-            settingsIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
-            PendingIntent pIntent = PendingIntent.getBroadcast(context, widgetID, settingsIntent, 0);
-            remoteViews.setOnClickPendingIntent(R.id.setting_button, pIntent);
+        try {
+            //new FeedResponceTask(context).execute("");
+            CommonStringsHelper res = new CommonStringsHelper(context);
+            jobs = RssReader.getLatestRssFeed(res);
+        } catch (Exception e) {
+            Log.e("RSS ERROR", "Error loading RSS Feed Stream >> " + e.getMessage() + " //" + e.toString());
         }
-        // Обновляем виджет
-        appWidgetManager.updateAppWidget(widgetID, remoteViews);
+
+        MainWidget.updateWidget(context, appWidgetManager, jobs);
     }
+
+    public static void updateWidget(Context context, AppWidgetManager appWidgetManager, List<JSONObject> jobs) {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main_widget_screen);
+
+        try {
+
+            if(!jobs.isEmpty()) {
+                Spanned title = (Spanned) jobs.get(0).get("text");
+
+                Spanned text = (Spanned) jobs.get(0).get("text");
+
+                remoteViews.setTextViewText(R.id.title, title);
+
+                remoteViews.setTextViewText(R.id.text, text);
+
+                //Intent intent = new Intent(context, RssActivity.class);
+
+                //PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+                //remoteViews.setOnClickPendingIntent(R.id.job_text, pendingIntent);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ComponentName rssWidget = new ComponentName(context, MainWidget.class);
+
+        appWidgetManager.updateAppWidget(rssWidget, remoteViews);
+    }
+
+//    @Override
+//    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+//        super.onUpdate(context, appWidgetManager, appWidgetIds);
+//        int widgetID = appWidgetIds[0];
+//        RemoteViews remoteViews;
+//        if (isSettings) {
+//            remoteViews = new RemoteViews(context.getPackageName(), R.layout.main_widget_screen);
+//            Intent countIntent = new Intent(context, MainWidget.class);
+//            countIntent.setAction(ACTION_CHANGE);
+//            countIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+//            PendingIntent pIntent = PendingIntent.getBroadcast(context, widgetID, countIntent, 0);
+//            remoteViews.setOnClickPendingIntent(R.id.button, pIntent);
+//        } else {
+//            remoteViews = new RemoteViews(context.getPackageName(), R.layout.main_widget_screen);
+//            remoteViews.setTextViewText(R.id.title, "Bugaga!");
+//            remoteViews.setTextViewText(R.id.text, "Bugagashenka!");
+//
+///*            Intent countIntent = new Intent(context, MainWidget.class);
+//            countIntent.setAction(ACTION_CHANGE);
+//            countIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+//            PendingIntent pIntent = PendingIntent.getBroadcast(context, widgetID, countIntent, 0);
+//            remoteViews.setOnClickPendingIntent(R.id.button_next, pIntent);*/
+//
+//            Intent settingsIntent = new Intent(context, MainWidget.class);
+//            settingsIntent.setAction(ACTION_OPEN_SETTINGS);
+//            settingsIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+//            PendingIntent pIntent = PendingIntent.getBroadcast(context, widgetID, settingsIntent, 0);
+//            remoteViews.setOnClickPendingIntent(R.id.setting_button, pIntent);
+//        }
+//        // Обновляем виджет
+//        appWidgetManager.updateAppWidget(widgetID, remoteViews);
+//    }
 
     @Override
     public void onEnabled(Context context) {
@@ -69,9 +127,14 @@ public class MainWidget extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        if(ACTION_OPEN_SETTINGS.equals(intent.getAction())){
+        if (ACTION_OPEN_SETTINGS.equals(intent.getAction())) {
             isSettings = true;
-        }else {
+//            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main_widget_screen);
+//            remoteViews.setInt(R.id.feed_screen, "visibility", View.GONE);
+//            remoteViews.setInt(R.id.settings_screen, "visibility", View.VISIBLE);
+//            AppWidgetManager.getInstance(context).updateAppWidget(
+//                    new ComponentName(context, MainWidget.class), remoteViews);
+        } else {
             isSettings = false;
         }
         Log.i("onReceive", "intent data" + intent.getAction());
